0537604480b78c0904e7ce09b6cf05754484d44e
RIRU <riruoda@gmail.com>
2023-06-10 19:03:12 +0900

    一文字出力

--- src/boot.s
+++ src/boot.s
@@ -15,6 +15,11 @@ ipl:
     mov sp, BOOT_LOAD
     sti                     ; 割り込みを有効化
     mov [BOOT.DRIVE], dl    ; ブートドライブを保存
+    mov ah, 0x0E            ; int 0x10(ah=0x0E)はテレタイプ式の文字列出力。
+    mov al, 'A'             ; 出力文字
+    mov bh, 0               ; ページ番号
+    mov bl, 0               ; 文字色(グラフィックスモードの時のみ有効)
+    int 0x10
     jmp $                   ; 無限ループ
 
 ALIGN 2, db 0               ; 0x90の代わりに0x00で埋める。
