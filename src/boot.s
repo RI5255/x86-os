89bad4e7d1d28e764ab51ba8de22cd2396657ada
RIRU <riruoda@gmail.com>
2023-06-10 19:54:17 +0900

    finish 14.4

--- src/boot.s
+++ src/boot.s
@@ -1,11 +1,14 @@
+    BOOT_LOAD   equ 0x7C00
+    ORG BOOT_LOAD
+
+%include "./include/macro.s"
+
 entry:
     jmp ipl
 
     times 90 - ($ - $$) db 0x90 ; BPB(Boot Parameter Block)
 
 ; IPL(Initial Program Loader)
-    BOOT_LOAD   equ 0x7C00
-    ORG BOOT_LOAD
 ipl:
     cli                     ; 割り込みを禁止
     mov ax, 0x0000
@@ -15,18 +18,14 @@ ipl:
     mov sp, BOOT_LOAD
     sti                     ; 割り込みを有効化
     mov [BOOT.DRIVE], dl    ; ブートドライブを保存
-    mov ah, 0x0E            ; int 0x10(ah=0x0E)はテレタイプ式の文字列出力。
-    mov al, 'A'             ; 出力文字
-    mov bh, 0               ; ページ番号
-    mov bl, 0               ; 文字色(グラフィックスモードの時のみ有効)
-    int 0x10
+    cdecl putc, 'A'
     jmp $                   ; 無限ループ
 
 ALIGN 2, db 0               ; 0x90の代わりに0x00で埋める。
 BOOT:
     .DRIVE: dw 0
 
+%include "./modules/real/putc.s"
+
     times 510 - ($ - $$) db 0x90
     db 0x55, 0xAA
-
-
---
+++ src/modules/real/putc.s
@@ -0,0 +1,15 @@
+; putc(char c);
+
+putc:
+    push bp 
+    mov bp, sp
+    push bx             ; bxはcallee saved register
+    mov ah, 0x0E        ; int 0x10(ah=0x0E)はテレタイプ式の文字列出力。
+    mov al, [bp + 4]    ; 文字コード
+    mov bh, 0           ; ページ番号
+    mov bl, 0           ; 文字色(グラフィックスモードの時のみ有効)
+    int 0x10
+    pop bx
+    mov sp, bp 
+    pop bp
+    ret
