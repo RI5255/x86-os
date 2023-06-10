ce4febd5122d0ce6aabe58427ee743f7f399c2c0
RIRU <riruoda@gmail.com>
2023-06-10 18:41:38 +0900

    finish 14.2

--- src/boot.s
+++ src/boot.s
@@ -1,5 +1,11 @@
 entry:
-    jmp $ ;無限ループ
+    jmp ipl
 
-    times 510 - ($ - $$) db 0x00
+    times 90 - ($ - $$) db 0x90 ; BPB(Boot Parameter Block)
+
+; IPL(Initial Program Loader)
+ipl:
+    jmp $ ; 無限ループ
+    times 510 - ($ - $$) db 0x90
     db 0x55, 0xAA
+
