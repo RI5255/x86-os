a637d6954992b0ad4edb0f869d6e0a31574bcebf
RIRU <riruoda@gmail.com>
2023-06-11 22:57:23 +0900

    finish 14.10

--- src/boot.s
+++ src/boot.s
@@ -43,17 +43,49 @@ BOOT:
     iend
 
 %include "./modules/real/puts.s"
-%include "./modules/real/itoa.s"
 %include "./modules/real/reboot.s"
 %include "./modules/real/read_chs.s"
 
     times 510 - ($ - $$) db 0
     db 0x55, 0xAA
 
-stage_2:
-    cdecl puts, .s0 
-    jmp $
-.s0     db "2nd stage...", 0x0A, 0x0D, 0
+%include "./modules/real/itoa.s"
+%include "./modules/real/get_drive_param.s"
 
-    times BOOT_SIZE - ($ - $$)   db 0
+stage_2:
+        cdecl puts, .s0
+
+        ; ドライブ情報を取得
+        cdecl get_drive_param, BOOT
+        cmp ax, 0
+        jne .L0
+        cdecl puts, .e0
+        call reboot
+.L0:    
+        ; ドライブ情報を表示
+        mov ax, [BOOT + drive.no]
+        cdecl itoa, ax, .p1, 2, 16, 0b0100
+        mov ax, [BOOT + drive.cyln]
+        cdecl itoa, ax, .p2, 4, 16, 0b0100
+        mov ax, [BOOT + drive.head]
+        cdecl itoa, ax, .p3, 2, 16, 0b0100
+        mov ax, [BOOT + drive.sect]
+        cdecl itoa, ax, .p4, 2, 16, 0b0100
+        cdecl puts, .s1
+        
+        ; 処理の終わり
+        jmp $
+
+        ; データ
+.s0     db "2nd stage...", 0x0A, 0x0D, 0
+.s1     db " Drive:0x"
+.p1     db "  , C:0x"
+.p2     db "    , H:0x"
+.p3     db "  , S:0x"
+.p4     db "  ", 0x0A, 0x0D, 0
+.e0     db "Can't get drive parameter.", 0
+
+        ; padding
+        times BOOT_SIZE - ($ - $$) db 0
+
 
---
+++ src/modules/real/get_drive_param.s
@@ -0,0 +1,47 @@
+; short get_drive_param(struct drive *d)
+; BIOSコールのINT 0x13(AH = 0x8)でドライブパラメータを取得できる。
+; この時取得できるのはアクセスできる最後のセクタのCHS。
+; 成功時0以外、失敗時0を返す。
+; rorはROtate Rightの略。
+
+get_drive_param:
+    push bp 
+    mov bp, sp 
+    
+    push bx 
+
+    mov si, [bp + 4]
+    
+    mov ax, 0
+    mov es, ax
+    mov di, ax 
+
+    mov ah, 8
+    mov dl, [si + drive.no]
+    int 0x13
+    jc  .L0         ; 失敗した場合はCF=1になる。
+
+    mov al, cl
+    and ax, 0x3F    ; 下位6bitのみ有効化(セクタ数)
+    shr cl, 6       ; シリンダ数の上位2bitのみにしておく。
+    ror cx, 8       ; シリンダ数
+    inc cx
+
+    movzx bx, dh    ; ヘッド数
+    inc bx  
+
+    mov [si + drive.cyln], cx 
+    mov [si + drive.head], bx 
+    mov [si + drive.sect], ax
+
+    jmp .L1
+
+.L0:
+    mov ax, 0
+
+.L1:
+    pop bx 
+    mov sp, bp 
+    pop bp 
+    ret
+
