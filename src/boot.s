63c93f79d99f982abe38fe5e315063d35c1ab450
RIRU <riruoda@gmail.com>
2023-06-11 22:01:24 +0900

    finish 14.9

--- src/boot.s
+++ src/boot.s
@@ -1,49 +1,53 @@
-    BOOT_LOAD   equ 0x7C00
-    ORG BOOT_LOAD
-
+%include "./include/define.s"
 %include "./include/macro.s"
+
+    ORG BOOT_LOAD
 
 entry:
     jmp ipl
-
-    times 90 - ($ - $$) db 0x90 ; BPB(Boot Parameter Block)
+    times 90 - ($ - $$) db 0x90     ; BPB(Boot Parameter Block)
 
 ; IPL(Initial Program Loader)
 ipl:
-    cli                     ; 割り込みを禁止
+    cli                             ; 割り込みを禁止
     mov ax, 0x0000
     mov ds, ax
     mov es, ax 
     mov ss, ax
     mov sp, BOOT_LOAD
-    sti                     ; 割り込みを有効化
-    mov [BOOT.DRIVE], dl    ; ブートドライブを保存
+    sti                             ; 割り込みを有効化
+    mov [BOOT + drive.no], dl       ; ブートドライブを保存
+    
     cdecl puts, .s0
-    mov ah, 0x2             ; セクタの読み出し
-    mov al, 1               ; 読み込みセクタ数
-    mov cx, 0x0002          ; シリンダ番号0, セクタ番号2
-    mov dh, 0x00            ; ヘッド番号0
-    mov dl, [BOOT.DRIVE]    ; ドライブ番号
-    mov bx, BOOT_LOAD + 512 ; 読み込みアドレス
-    int 0x13                ; 成功時CF=0, 失敗時CF=1
-    jnc .L0
-    cdecl puts, .e0         ; 失敗した場合はエラー表示して再起動
+    
+    mov bx, BOOT_SECT - 1           ; 残りのセクタ数
+    mov cx, BOOT_LOAD + SECT_SIZE   ; 次のロードアドレス 
+    cdecl read_chs, BOOT, bx, cx    
+    cmp ax, bx
+    jz  .L0
+    cdecl puts, .e0                 ; 失敗した場合はエラー表示して再起動
     call reboot
 .L0:
-    jmp stage_2             ; ブートの第二ステージに飛ぶ
+    jmp stage_2                     ; ブートの第二ステージに飛ぶ
 
 .s0     db "Booting...", 0x0A, 0x0D, 0
 .e0     db "Error:sector read", 0
 
-ALIGN 2, db 0               ; 0x90の代わりに0x00で埋める。
+ALIGN 2, db 0                       ; 0x90の代わりに0x00で埋める。
 BOOT:
-    .DRIVE: dw 0
+    istruc drive
+        at drive.no,    dw 0        ; ドライブ番号
+        at drive.cyln,  dw 0        ; シリンダ
+        at drive.head,  dw 0        ; ヘッド
+        at drive.sect,  dw 2        ; セクタ　
+    iend
 
 %include "./modules/real/puts.s"
 %include "./modules/real/itoa.s"
 %include "./modules/real/reboot.s"
+%include "./modules/real/read_chs.s"
 
-    times 510 - ($ - $$) db 0x90
+    times 510 - ($ - $$) db 0
     db 0x55, 0xAA
 
 stage_2:
@@ -51,4 +55,5 @@ stage_2:
     jmp $
 .s0     db "2nd stage...", 0x0A, 0x0D, 0
 
-    times 1024 * 8 - ($ - $$)   db 0
+    times BOOT_SIZE - ($ - $$)   db 0
+
---
+++ src/modules/real/read_chs.s
@@ -0,0 +1,44 @@
+; short read_chs(struct drive *d, short sect, char *dst);
+; CHS方式で指定されたセクタを読み出す関数
+read_chs:
+    push bp 
+    mov bp, sp 
+    
+    push 3                      ; リトライ回数 
+    
+    push bx
+    
+    mov si, [bp + 4] ; 構造体のアドレス
+    
+    mov ch, [si + drive.cyln]   ; シリンダ番号(下位8bit)
+    mov cl, [si + drive.cyln + 1] 
+    shl cl, 6                   ; シリンダ番号(上位2bit)
+    or cl, [si + drive.sect]    ; セクタ番号 
+    mov dh, [si + drive.head]   ; ヘッダ番号
+    mov dl, [si]                ; ドライブ番号
+    mov ax, 0                  
+    mov es, ax                  ; int 0x13で読み込むときは、es:bxに読み込まれる
+    mov bx, [bp + 8]            ; 読み出し先アドレス。
+
+.L0:
+    mov ah, 0x02
+    mov al, [bp + 6]            ; 読み出しセクタ数
+    int 0x13    
+    jnc .L1                     ; CF=0なら成功。1なら失敗
+    mov al, 0
+    jmp .L2                     ; 失敗ならそのまま戻る。
+
+.L1: 
+    cmp al, 0                   ; alには読み出したセクタ数が入る
+    jne .L2                     ; 読み込めていれば終了
+    mov ax, 0
+    dec word [bp - 2]
+    jnz .L0                     ; リトライを試みる    
+
+.L2: 
+    mov ah, 0                   ; ahにはステータスコードが入っているが破棄。(返り値は読み込んだセクタ数になる。)
+
+    pop bx
+    mov sp, bp 
+    pop bp 
+    ret
